45c7eff33200309ca8e5a166dab2b7f93a92f598
unknown <stivengar20088@gmail.com>
2024-10-07 21:05:52 -0500

    url

--- src/api/book.api.js
+++ src/api/book.api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //Instancia para el back
 const booksApi = axios.create({
-  // baseURL: "http://127.0.0.1:8000/books/api/v1/books/",//Desarrollo
-    baseURL: "https://bookappbackend-31ic.onrender.com/books/api/v1/"
+  //baseURL: "http://127.0.0.1:8000/books/api/v1/books/",//Desarrollo
+    baseURL: "https://bookappbackend-31ic.onrender.com/books/api/v1/books/"
 });
 //Solicitudes endpoint de la API
 
